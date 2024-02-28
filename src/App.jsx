5f1e89ee07271574a1f6f47885ffd93dc4ba30fe
arebac <j.cabreraortiz@outlook.com>
2024-02-28 09:01:05 -0400

    Added Navbar to Page

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+    <Footer/>
+    <Sidebar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <div class="topnav">
+      <a class="active" href="#home">
+        To Do List App
+      </a>
+      <div class="logo">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Microsoft_To-Do_icon.png/1470px-Microsoft_To-Do_icon.png?20200320172308" />
+      </div>
+
+      {/* <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a> */}
+    </div>
+  );
+};
+
+export default Navbar;
